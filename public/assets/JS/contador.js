57223dbdb600385314a6b8e1e2dc0ee6e0269014
PalomaSelva <palomaselva1@gmail.com>
2022-08-03 11:58:32 -0300

    ITEM | Criando contador + botão "COMPRAR"

---
+++ public/assets/JS/contador.js
@@ -0,0 +1,16 @@
+var currentNumberWrapper = window.document.getElementById('currentNumber')
+var currentNumber = 1;
+currentNumberWrapper.innerHTML=currentNumber
+
+function decrement(){
+      currentNumber-=1;
+      if (currentNumber<=1) {
+            currentNumber =  1;
+      }
+      currentNumberWrapper.innerHTML=currentNumber;
+}
+
+function increment(){
+      currentNumber+=1;
+      currentNumberWrapper.innerHTML=currentNumber;
+}
