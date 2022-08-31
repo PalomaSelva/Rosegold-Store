e08405705d1050d3beb92a54bc2751911a95f1de
PalomaSelva <palomaselva1@gmail.com>
2022-08-31 10:57:06 -0300

    mudando nome de classe "header-first" para "header-one"

--- public/assets/JS/contador2.js
+++ public/assets/JS/contador2.js
@@ -1,11 +1,20 @@
 for (let i = 0; i < 2; i++) {
       var numeroAtual = ".currentNumber" + i;
       currentNumberWrapper = window.document.querySelector(numeroAtual)
-      console.log(currentNumberWrapper)
+
       var currentNumber = 1;
       currentNumberWrapper.innerHTML=currentNumber
 
 for (let i = 0; i < 2; i++) {
+      idButtonAdd = "#add" + i;
+      increment = window.document.querySelector(idButtonAdd)
+      increment.onclick = function(){
+            currentNumber+=1;
+            currentNumberWrapper.innerHTML=currentNumber;
+            console.log(currentNumberWrapper)
+
+      }
+      
       idButtonSub = "#sub" + i;
       decrement = window.document.querySelector(idButtonSub)
       decrement.onclick = function(){
@@ -18,13 +27,6 @@ for (let i = 0; i < 2; i++) {
             currentNumberWrapper.innerHTML=currentNumber;
       }
 
-      idButtonAdd = "#add" + i;
-      increment = window.document.querySelector(idButtonAdd)
-      increment.onclick = function(){
-            currentNumber+=1;
-            currentNumberWrapper.innerHTML=currentNumber;
-
-      }
 }
 
 }
