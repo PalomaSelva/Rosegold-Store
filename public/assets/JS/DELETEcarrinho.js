753867c1ab725e54e67371c9a1fb52bbd4f99a8f
Paloma Selva <palomaslva1@gmail.com>
2022-10-26 10:15:53 -0300

    Carrinho estático finalizado

---
+++ public/assets/JS/DELETEcarrinho.js
@@ -0,0 +1,9 @@
+//se tiver +2 <tr>
+/* 
+const table = window.document.querySelector('table tbody')
+const prodList = window.document.querySelector('table tr:not(.table-header)')
+console.log(prodList)
+
+if (table.childElementCount > 2) {
+    
+} */
